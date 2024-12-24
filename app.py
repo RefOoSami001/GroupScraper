837998bc1f9acb8e8f4b3de3ad7aab2c15e4c169
RefOoSami001 <159632003+RefOoSami001@users.noreply.github.com>
2024-12-24 12:05:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -202,7 +202,6 @@ def logout():
     flash('Logged out successfully', 'success')
     return redirect(url_for('login'))
 
-# Verification code finder route
 @app.route('/verification_code_finder', methods=['GET', 'POST'])
 def verification_code_finder():
     if not session.get('logged_in'):
@@ -213,6 +212,8 @@ def verification_code_finder():
             try:
                 numbers = request.form['numbers'].split()
                 selected_api = request.form.get('api')
+                phpsessid = request.form.get('phpsessid', None)  # Get PHPSESSID if provided
+                print(phpsessid)
 
                 total_success = 0
                 total_fail = 0
@@ -236,14 +237,19 @@ def verification_code_finder():
                         code = get_panel_code_api7(number)
                     elif selected_api == '8':
                         code = get_panel_code_api8(number)
-                    elif selected_api == '9':
-                        code = get_panel_code_api9(number)
+                    elif selected_api == '9':  # For +966 API
+                        if not phpsessid:
+                            flash('PHPSESSID is required for +966 API.', 'danger')
+                            return render_template('verification.html')
+                        code = get_panel_code_api9(number, phpsessid)  # Pass PHPSESSID
                     elif selected_api == '10':
                         code = get_panel_code_api10(number)
                     elif selected_api == '11':
                         code = get_panel_code_api11(number)
                     elif selected_api == '12':
                         code = get_panel_code_api12(number)
+                    elif selected_api == '13':
+                        code = get_panel_code_api13(number)
                     else:
                         flash('Please select an API.', 'danger')
                         return render_template('verification.html')
@@ -719,9 +725,10 @@ def get_panel_code_api8(number):
         'capt': str(result),
     }
 
-    response = requests.post('http://93.190.143.35/ints/signin', cookies=cookies, headers=headers, data=data, verify=False)
+    response2 = requests.post('http://93.190.143.35/ints/signin', cookies=cookies, headers=headers, data=data)
+
     cookies = {
-        'PHPSESSID': response1.cookies['PHPSESSID'],
+        'PHPSESSID': response2.cookies['PHPSESSID'],
     }
 
     headers = {
@@ -753,7 +760,7 @@ def get_panel_code_api8(number):
             return None
     except:
         return None
-def get_panel_code_api9(number):
+def get_panel_code_api9(number,phpsessid):
     # headers = {
     #     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     #     'Accept-Language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
@@ -792,7 +799,7 @@ def get_panel_code_api9(number):
     # response2 = requests.post('http://109.236.84.81/ints/signin', cookies=cookies, headers=headers, data=data,timeout=30)
 
     cookies = {
-        'PHPSESSID': 'tmp6k9uqqvm7vgi8k7l86a3qeb',
+        'PHPSESSID': phpsessid,
     }
 
     headers = {
@@ -852,7 +859,7 @@ def get_panel_code_api12(number):
         return response.text
     
     print("Your IP address is:", get_ip())
-    token = 'tqimpuIULab9dtcbRssavrhuShijdNupBzjhNnxa62d80b40'
+    token = 'W8SPf83NqwGy2LvRZHYMBoLK890fFEpkW3k5hO3Reec0ae89'
     url = f'https://www.ivasms.com/api/sms?to={number}'
 
     # Define headers
@@ -875,6 +882,52 @@ def get_panel_code_api12(number):
             return None
     except requests.exceptions.RequestException as e:
         return None
+def get_panel_code_api13(number):
+    cookies = {
+        'XSRF-TOKEN': 'eyJpdiI6ImpxTmxLaUsrTUlNZjBCWXFWVWhGQlE9PSIsInZhbHVlIjoiVkdNWnYvM2VxSXF1enlLd0oxM2laeElwRDlPNWIzTmcvWHlBbHkyRXhENnFaWWtBUUNTZ1VpUHpYd0JpbWVNdFBLeUFCc05xL2JXWEpkNGcvdGllUUtIdjJQOHlVdnZ3dzBLeitUc1NiUlB1ZUxhTHV0OWtBbmN4Q3BzOFlaczMiLCJtYWMiOiIwM2Q5OThjYmMwY2ZiMWU4ZmZjYmM3ZGQ2ZjcyMTczNzUyNTE2YjRmOGFiMmFjN2U3YzY4YThkYjhjNDhiYWFjIiwidGFnIjoiIn0%3D',
+        'basha_iprn_vas_session': 'eyJpdiI6IjhWUmZ0M2xKQ09aV0NSaGI5ZWt0NGc9PSIsInZhbHVlIjoiOUNBaUNERzFJRWRtT00yRlV2SVJDQzNlTnFDZlhCRnlWVnYrUHlwb3NmZ2RXUWFvVzJOWERqMU9HckcxNEQyclpNcmR5VFZFOXQ3THRENU1wazZUQXVlRVpwTVBVbm1icmU4K3RuMEp6NVBNbnNwVzF3dURxb2dUSjBhY05lVVAiLCJtYWMiOiI0NDYxMWMwMzZmOWZlNmI1Y2FiNWZiNTcyNjAxNDZmMzI4MTJmNWRhNWZjOGFmMmFkZGZmMjlmNzY3NjgwMGQ0IiwidGFnIjoiIn0%3D',
+    }
+
+    headers = {
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+        'priority': 'u=1, i',
+        'referer': 'https://basha.cc/my/messages',
+        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest',
+    }
+
+    params = {
+        'draw': '4',
+        'columns[0][data]': 'country_iso_name',
+        'columns[1][data]': 'destination_number',
+        'columns[2][data]': 'source_number',
+        'columns[3][data]': 'message_text',
+        'columns[4][data]': 'message_count',
+        'columns[5][data]': 'client_revenue',
+        'columns[6][data]': 'timeStamp',
+        'order[0][column]': '1',
+        'order[0][dir]': 'desc',
+        'start': '0',
+        'length': '10',
+        'search[value]': str(number),
+        '_': '1735033239633',
+    }
+
+    response = requests.get('https://basha.cc/my/messages', params=params, cookies=cookies, headers=headers)
+    data = response.json()
+    if data['data']:
+        last_message = data['data'][0]['message_text']
+        match = re.search(r'\b\d{5}\b', last_message)
+        if match:
+            return match.group()
+    return None
 
 def get_verification_code3(number, user, password):
 
@@ -883,12 +936,12 @@ def get_verification_code3(number, user, password):
         'Accept-Language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
         'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive',
-        'Referer': 'http://91.232.105.47/NumberPanel/agent/SMSDashboard',
+        'Referer': 'http://51.178.79.51/NumberPanel/agent/SMSDashboard',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
     }
 
-    response1 = requests.get('http://91.232.105.47/NumberPanel/login', headers=headers)
+    response1 = requests.get('http://51.178.79.51/NumberPanel/login', headers=headers)
     num1, num2 = map(int, re.findall(r'\d+', BeautifulSoup(response1.text, 'html.parser').get_text()))
     result = num1 + num2
     cookies = {
@@ -901,8 +954,8 @@ def get_verification_code3(number, user, password):
         'Connection': 'keep-alive',
         'Content-Type': 'application/x-www-form-urlencoded',
         # 'Cookie': 'PHPSESSID=ag67srbm45h1f0b2vf7u58t0qb',
-        'Origin': 'http://91.232.105.47',
-        'Referer': 'http://91.232.105.47/NumberPanel/login',
+        'Origin': 'http://51.178.79.51',
+        'Referer': 'http://51.178.79.51/NumberPanel/login',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
     }
@@ -913,7 +966,7 @@ def get_verification_code3(number, user, password):
         'capt': str(result),
     }
 
-    response = requests.post('http://91.232.105.47/NumberPanel/signin', cookies=cookies, headers=headers, data=data, verify=False)
+    response = requests.post('http://51.178.79.51/NumberPanel/signin', cookies=cookies, headers=headers, data=data, verify=False)
     cookies = {
         'PHPSESSID': response1.cookies['PHPSESSID'],
     }
@@ -923,14 +976,14 @@ def get_verification_code3(number, user, password):
         'Accept-Language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
         'Connection': 'keep-alive',
         # 'Cookie': 'PHPSESSID=kvbs85l6e2ne81feafqngih2ff',
-        'Referer': 'http://91.232.105.47/NumberPanel/agent/SMSCDRReports',
+        'Referer': 'http://51.178.79.51/NumberPanel/agent/SMSCDRReports',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
         'X-Requested-With': 'XMLHttpRequest',
     }
 
     # Get the current date in the required format (YYYY-MM-DD HH:MM:SS)
     current_date = datetime.now().strftime('%Y-%m-%d')
-    response = requests.get(f'http://91.232.105.47/NumberPanel/agent/res/data_smscdr.php?fdate1=2024-12-08%2000:00:00&fdate2={current_date}%2023:59:59&frange=&fclient=&fnum={number}&fcli=&fgdate=&fgmonth=&fgrange=&fgclient=&fgnumber=&fgcli=&fg=0&sEcho=1&iColumns=9&sColumns=%2C%2C%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=25&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&mDataProp_7=7&sSearch_7=&bRegex_7=false&bSearchable_7=true&bSortable_7=true&mDataProp_8=8&sSearch_8=&bRegex_8=false&bSearchable_8=true&bSortable_8=false&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=1733682941115',
+    response = requests.get(f'http://51.178.79.51/NumberPanel/agent/res/data_smscdr.php?fdate1=2024-12-08%2000:00:00&fdate2={current_date}%2023:59:59&frange=&fclient=&fnum={number}&fcli=&fgdate=&fgmonth=&fgrange=&fgclient=&fgnumber=&fgcli=&fg=0&sEcho=1&iColumns=9&sColumns=%2C%2C%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=25&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&mDataProp_7=7&sSearch_7=&bRegex_7=false&bSearchable_7=true&bSortable_7=true&mDataProp_8=8&sSearch_8=&bRegex_8=false&bSearchable_8=true&bSortable_8=false&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=1733682941115',
                         cookies=cookies, headers=headers)
 
     try:
